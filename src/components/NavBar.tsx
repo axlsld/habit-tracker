39e69ac6b46802bebe9eef26a24e0c02e3d67cf5
alexis <johnalexissaldua06@gmail.com>
2026-06-09 16:56:36 +0800

    Add navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <div className="title">Habit Tracker</div>
+      <div className="nav-item">Dashboard</div>
+      <div className="nav-item">Habits</div>
+      <div className="nav-item">Sample</div>
+    </div>
+  );
+};
+
+export default NavBar;
